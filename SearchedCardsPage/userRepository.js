08aef4c6f962d65691c71ac5773e4913983cac5f
vladimir-koloski <v.koloski1@gmail.com>
2020-05-28 15:07:08 +0200

    change in code

--- SearchedCardsPage/userRepository.js
+++ SearchedCardsPage/userRepository.js
@@ -1,7 +1,7 @@
 
 let searchRoomates = document.getElementById("search-btn");
-let cardsRoomateDiv = document.getElementsByClassName("card-wrapper")[0];
-let results = [];
+let cardsRoomateDiv = document.getElementsByClassName("wrapper")[0];
+let roomates = [];
 
 let RoomatesDb = {
     getAll: function() {
@@ -9,20 +9,59 @@ let RoomatesDb = {
                 "https://raw.githubusercontent.com/sedc-codecademy/sp2020-cp09-afi/develop/Jsons/users.json"
             )
             .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                results = data;
-                
-                createUserCard(data, cardsRoomateDiv);
+            .then(data => {                
+              roomates = data;
+              console.log(data);
+              console.log(roomates);
+              $('#list').pagination({
+                dataSource: roomates,
+                pageSize: 8,
+                callback: function(data, pagination) {                    
+                    
+                    let wrapper = $('#list .wrapper').empty();
+                    $.each(data, function (i, f) {
+                      const imageMale = "images/tirkizna3.png";
+                      const imageFemale = "images/magenta.png";
+                      const colorMale = "card card-color-2";
+                      const colorFemale = "card card-color-1";
+                       $('#list .wrapper').append(`<div class="${f.user.sex === 'жена' ? colorFemale :colorMale}">
+                       <div class="card-house-icon">
+                         <img src="${f.user.sex === 'жена' ? imageFemale : imageMale}" alt="" class="card-house-image">
+                       </div>
+                       <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 203.89 171.94">
+                       <clipPath id="House"> 
+                         <style>.cls-1{fill:none;stroke:none;stroke-miterlimit:10;stroke-width:8px;}</style>
+                         <polygon class="cls-1"  points="9.66 96.25 35.27 96.25 35.27 167.94 167.13 167.94 167.13 96.25 194.01 96.25 100.41 5.61 9.66 96.25"/>
+                       </clipPath>
+                     <polygon class="cls-1" points="9.66 96.25 35.27 96.25 35.27 167.94 167.13 167.94 167.13 96.25 194.01 96.25 100.41 5.61 9.66 96.25"/>
+                     <image  class ="imageinhouse"width="200" height="200" xlink:href=${f.user.image}" clip-path="url(#House)" />
+                     </svg>
+                       
+                       
+                       <div class="card-body">
+                         <h4 class="card-title-2">${f.user.fullName}</h4>
+                         <span class="card-text-2">23, Карпош 3</span><br>
+                         <a href="#" class="btn btn-primary">Порака</a>
+                       </div>
+                     </div>`);
+                    });
+                  }
+                 });
+              
+              console.log(roomates);                
+              //createUserCard(data, cardsRoomateDiv);
 
             })
             .catch(error => console.log(error));
     },
 };
 
+setTimeout(function(){console.log(roomates)}, 10000);
+
 searchRoomates.addEventListener("click", function(){
     RoomatesDb.getAll();
 });
+
 
 
 
@@ -53,74 +92,19 @@ function createUserCard(roomates, element) {
     }
 };
 
-// var url = "https://spreadsheets.google.com/feeds/list/1xo6dUfcVOwPA5Lkd8pKevLj6lP-0gOaPXNBZk4jyuGw/od6/public/values?alt\u003djson";
 
-// var perPage = 3;
-// var currentPage = 1;
-// var values = []; // Added
 
-// // Modified
-// $.getJSON(url, function(data) {
-//   var element = "";
-//   $.each(data.feed.entry, function(index,value) {
-//     values.push({Category: value.gsx$largeimage.$t, Name: value.gsx$imagetitle.$t});
-//     if (index < perPage) {
-//       for (const roomate of roomates) {
-//                 console.log(roomate.user)
-//                 const imageMale = "images/tirkizna3.png";
-//                 const imageFemale = "images/magenta.png";
-//                 const colorMale = "card card-color-2";
-//                 const colorFemale = "card card-color-1";
-//                 element.innerHTML += `<div class="${roomate.user.sex === 'жена' ? colorFemale :colorMale}">
-//                 <div class="card-house-icon">
-//                   <img src="${roomate.user.sex === 'жена' ? imageFemale : imageMale}" alt="" class="card-house-image">
-//                 </div>
-        
-//                 <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 203.89 171.94">
-//                   <image  class="imageinhouse" xlink:href="${roomate.user.image}" clip-path="url(#House)" />
-//                 </svg>
-                
-//                 <div class="card-body">
-//                   <h4 class="card-title-2">${roomate.user.fullName}</h4>
-//                   <span class="card-text-2">23, Карпош 3</span><br>
-//                   <a href="#" class="btn btn-primary">Порака</a>
-//                 </div>
-//               </div>`
-                     
-//             }
-//       element += "Category: " + value.gsx$largeimage.$t + "</br>"+
-//                 "Name: " + value.gsx$imagetitle.$t + "</br></br>";
-//     }
-//   });
-//   $(".test").append(element);
+
+
+//    $.getJSON('https://raw.githubusercontent.com/sedc-codecademy/sp2020-cp09-afi/develop/Jsons/users.json', function (json) {
+//     $('#list').pagination({
+//         dataSource: json.Product,
+//         pageSize: 2,
+//         callback: function(data, pagination) {
+//             var wrapper = $('#list .wrapper').empty();
+//             $.each(data, function (i, f) {
+//                 $('#list .wrapper').append('<ul><li>Key1: ' + f.Key1 + '</li></ul>');
+//             });
+//         }
+//     });
 // });
-
-// function prevPage() {
-//   if (currentPage > 1) {
-//     currentPage--;
-//     changePage();
-//   }
-// }
-
-// function nextPage() {
-//   if (currentPage < numPages()) {
-//     currentPage++;
-//     changePage();
-//   }
-// }
-
-// // Modified
-// function changePage() {
-//   $(".test").html("");
-//   var start = (currentPage - 1) * perPage;
-//   var output = values.slice(start, start + perPage).reduce((s, e) => {
-//     return s += "Category: " + e.Category + "</br>"+
-//                 "Name: " + e.Name + "</br></br>";    
-//   }, "");
-//   $(".test").append(output);
-// }
-
-// // Modified
-// function numPages() {
-//   return Math.ceil(values.length / perPage);
-// }
