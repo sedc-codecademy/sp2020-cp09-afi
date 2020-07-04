9bd511052b4cbe1c9f61a703a66a44d0b8e9bb97
vladimir-koloski <v.koloski1@gmail.com>
2020-07-04 17:36:49 +0200

    change

--- SearchedCardsPage/searchcards.js
+++ SearchedCardsPage/searchcards.js
@@ -55,9 +55,89 @@ search.addEventListener("click", function(){
     
 });
 
-function perfectMatch(){
-    
+function showDivPerfectMatch(){
+    homepage.style.display = 'none';  
+    searchApartments.style.display = 'none';
+    userprofilepage.style.display = 'none';
+    trys.style.display = "none";      
+    searchCards.style.display = 'block';
+
 }
+let perfectMatchList = [];
+let perfectMatchBtn = document.getElementById('sovpagjanja-page1');
+
+
+let RoomatesDb2 = {
+    getAll: function() {
+        fetch(
+                "https://raw.githubusercontent.com/sedc-codecademy/sp2020-cp09-afi/develop/Jsons/users.json"
+            )
+            .then(res => res.json())
+            .then(data => {     
+              console.log(data);           
+              
+              let region2 = roomateUser[2].regionUser;
+            let isSmoker2 = roomateUser[1].isSmoker1;
+            console.log(region2);
+            console.log(isSmoker2);
+              
+              perfectMatchList = perfectMatch(data, region2, isSmoker2);
+              console.log(perfectMatchList, "filtered list ");
+              paginationInitPerfectMatch();                   
+            })
+            .catch(error => console.log(error));
+    },
+};
+
+function perfectMatch(roomates, region2, isSmoker2){
+    
+        return roomates.filter(r => region2 === r.preferences.roomQuestions[0].region
+        //&& isSmoker2 == String(r.preferences.smoker[0].isSmoker)
+      ); 
+}
+perfectMatchBtn.addEventListener('click', function(){
+    RoomatesDb2.getAll();
+    showDivPerfectMatch();
+})
+
+function paginationInitPerfectMatch (){
+    $('#list').pagination({
+      dataSource: perfectMatchList,
+      pageSize: 10,
+      callback: function(data, pagination) {                    
+          console.log(data, "pagination");
+          let wrapper = $('#list .wrapper').empty();
+          $.each(data, function (i, roomate) {
+            
+            // if(selectedLocation === roomate.preferences.roomQuestions[0].region.trim()){
+              const imageMale = "./SearchedCardsPage/images/tirkizna3.png";
+              const imageFemale = "./SearchedCardsPage/images/magenta.png";
+              const colorMale = "card card-color-2";
+              const colorFemale = "card card-color-1";
+               $('#list .wrapper').append(`<div class="${roomate.user.sex === 'жена' ? colorFemale :colorMale}">
+               <div class="card-house-icon">
+                 <img src="${roomate.user.sex === 'жена' ? imageFemale : imageMale}" alt="" class="card-house-image">
+               </div>
+               <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 203.89 171.94">
+               <clipPath id="Houses"> 
+                 <polygon class="cls-5"  points="9.66 96.25 35.27 96.25 35.27 167.94 167.13 167.94 167.13 96.25 194.01 96.25 100.41 5.61 9.66 96.25"/>
+               </clipPath>
+             <polygon class="cls-5" points="9.66 96.25 35.27 96.25 35.27 167.94 167.13 167.94 167.13 96.25 194.01 96.25 100.41 5.61 9.66 96.25"/>
+             <image  class ="imageinhouse" xlink:href=${roomate.user.image}" clip-path="url(#Houses)" />
+             </svg>            
+               <div class="card-body">
+               <h4 class="card-title-2">${roomate.user.fullName}</h4>
+               <span class="card-text-2">${roomate.user.age}, ${roomate.preferences.roomQuestions[0].region}</span><br>
+               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+               Порака
+             </button>             </div>
+             </div>`);
+            // }
+            
+          });
+        }
+       });
+  }
 
 let getwrap = document.querySelector(".wrapper");
 
